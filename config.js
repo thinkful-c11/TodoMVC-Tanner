df50a414574896d5def23cf5ef433849a5183f88
tannaurus <contacttannergill@gmail.com>
2017-06-02 12:31:45 -0700

    updated database link.

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 'use strict';
 
 require('dotenv').config();
-const DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL;
+const DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL || 'postgres://zarbeteg:E66VH_mjBv8O6nXk17zP2C7X_oB2OXMx@stampy.db.elephantsql.com:5432/zarbeteg';
 
 exports.DATABASE = {
   client: 'pg',
